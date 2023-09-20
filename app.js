ec1105eb4c64e79ae7946a7139f87625c6fe060e
GROWTHDM <135638520+GROWTHDM@users.noreply.github.com>
2023-09-20 14:55:54 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 fetch('./pressel-ninjacrash.json')
     .then((response) => response.json())
     .then((json) => console.log(json));
+
+btn.addEventListener('click', () => {
+    window.open("https://www.redirectmais.com/run/comunidadedoce");
+})
